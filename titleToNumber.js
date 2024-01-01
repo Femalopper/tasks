f6216b8deb93546555c8d5816d31a5e2662be342
Ангелина Головенко <lina-kursk@mail.ru>
2024-01-01 18:32:37 +0300

    add blank line

--- titleToNumber.js
+++ titleToNumber.js
@@ -1,5 +1,6 @@
 // This file contains solution to the following problem:
 // https://leetcode.com/problems/excel-sheet-column-number/
+
 const titleToNumber = (columnTitle) => {
   const alphabet = [
     "A",
